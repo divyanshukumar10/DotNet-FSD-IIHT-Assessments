da544c95df86494baf37c34fb47bb31cc31696cc
divyanshukumar10 <divyanshu.kr10@gmail.com>
2023-05-01 15:19:23 +0530

    assignment 4 1

---
+++ .NET - M4 Assessment 1/Problem 1/ConsoleApp1/Program.cs
@@ -0,0 +1,153 @@
+﻿internal abstract class Fighter
+{
+	internal KickBehavior kickBehavior;
+
+	internal JumpBehavior jumpBehavior;
+
+	public Fighter(KickBehavior kickBehavior, JumpBehavior jumpBehavior)
+	{
+		this.jumpBehavior = jumpBehavior;
+		this.kickBehavior = kickBehavior;
+	}
+
+	public virtual void punch()
+	{
+		System.Console.Out.WriteLine("Default Punch");
+	}
+
+	public virtual void kick()
+	{
+		// delegate to kick behavior
+		kickBehavior.kick();
+	}
+
+	public virtual void jump()
+	{
+		// delegate to jump behavior
+		jumpBehavior.jump();
+	}
+
+	public virtual void roll()
+	{
+		System.Console.Out.WriteLine("Default Roll");
+	}
+
+	public virtual void setKickBehavior(KickBehavior kickBehavior)
+	{
+		this.kickBehavior = kickBehavior;
+	}
+
+	public virtual void setJumpBehavior(JumpBehavior jumpBehavior)
+	{
+		this.jumpBehavior = jumpBehavior;
+	}
+
+	public abstract void display();
+}
+
+internal interface KickBehavior
+{
+	// Encapsulated kick behaviors
+	void kick();
+}
+
+internal class LightningKick : KickBehavior
+{
+	public virtual void kick()
+	{
+		System.Console.Out.WriteLine("Lightning Kick");
+	}
+}
+
+internal class TornadoKick : KickBehavior
+{
+	public virtual void kick()
+	{
+		System.Console.Out.WriteLine("Tornado Kick");
+	}
+}
+
+internal interface JumpBehavior
+{
+	// Encapsulated jump behaviors
+	void jump();
+}
+
+internal class ShortJump : JumpBehavior
+{
+	public virtual void jump()
+	{
+		System.Console.Out.WriteLine("Short Jump");
+	}
+}
+
+internal class LongJump : JumpBehavior
+{
+	public virtual void jump()
+	{
+		System.Console.Out.WriteLine("Long Jump");
+	}
+}
+
+internal class Ryu : Fighter
+{
+	public Ryu(KickBehavior kickBehavior, JumpBehavior jumpBehavior)
+		: base(kickBehavior, jumpBehavior)
+	{
+	}
+
+	// Characters
+	public override void display()
+	{
+		System.Console.Out.WriteLine("Ryu");
+	}
+}
+
+internal class Ken : Fighter
+{
+	public Ken(KickBehavior kickBehavior, JumpBehavior jumpBehavior)
+		: base(kickBehavior, jumpBehavior)
+	{
+	}
+
+	public override void display()
+	{
+		System.Console.Out.WriteLine("Ken");
+	}
+}
+
+internal class ChunLi : Fighter
+{
+	public ChunLi(KickBehavior kickBehavior, JumpBehavior jumpBehavior)
+		: base(kickBehavior, jumpBehavior)
+	{
+	}
+
+	public override void display()
+	{
+		System.Console.Out.WriteLine("ChunLi");
+	}
+}
+
+internal class StreetFighter
+{
+	// Driver class
+	public static void Main(string[] args)
+	{
+		// let us make some behaviors first
+		JumpBehavior shortJump = new ShortJump();
+		JumpBehavior LongJump = new LongJump();
+		KickBehavior tornadoKick = new TornadoKick();
+		// Make a fighter with desired behaviors
+		Fighter ken = new Ken(tornadoKick, shortJump);
+		ken.display();
+		// Test behaviors
+		ken.punch();
+		ken.kick();
+		ken.jump();
+		// Change behavior dynamically (algorithms are
+		// interchangeable)
+		ken.setJumpBehavior(LongJump);
+		ken.jump();
+	}
+}
---
+++ .NET - M4 Assessment 1/Problem 2/ConsoleApp1/Program.cs
@@ -0,0 +1,104 @@
+﻿internal class ATCMediator : IATCMediator
+{
+	private Flight flight;
+
+	private Runway runway;
+
+	public bool land;
+
+	public virtual void registerRunway(Runway runway)
+	{
+		this.runway = runway;
+	}
+
+	public virtual void registerFlight(Flight flight)
+	{
+		this.flight = flight;
+	}
+
+	public virtual bool isLandingOk()
+	{
+		return land;
+	}
+
+	public virtual void setLandingStatus(bool status)
+	{
+		land = status;
+	}
+}
+
+internal interface Command
+{
+	void land();
+}
+
+internal interface IATCMediator
+{
+	void registerRunway(Runway runway);
+
+	void registerFlight(Flight flight);
+
+	bool isLandingOk();
+
+	void setLandingStatus(bool status);
+}
+
+internal class Flight : Command
+{
+	private IATCMediator atcMediator;
+
+	public Flight(IATCMediator atcMediator)
+	{
+		this.atcMediator = atcMediator;
+	}
+
+	public virtual void land()
+	{
+		if (atcMediator.isLandingOk())
+		{
+			System.Console.Out.WriteLine("Successfully Landed.");
+			atcMediator.setLandingStatus(true);
+		}
+		else
+		{
+			System.Console.Out.WriteLine("Waiting for landing.");
+		}
+	}
+
+	public virtual void getReady()
+	{
+		System.Console.Out.WriteLine("Ready for landing.");
+	}
+}
+
+internal class Runway : Command
+{
+	private IATCMediator atcMediator;
+
+	public Runway(IATCMediator atcMediator)
+	{
+		this.atcMediator = atcMediator;
+		atcMediator.setLandingStatus(true);
+	}
+
+	public virtual void land()
+	{
+		System.Console.Out.WriteLine("Landing permission granted.");
+		atcMediator.setLandingStatus(true);
+	}
+}
+
+internal class MediatorDesignPattern
+{
+	public static void Main(string[] args)
+	{
+		IATCMediator atcMediator = new ATCMediator();
+		Flight sparrow101 = new Flight(atcMediator);
+		Runway mainRunway = new Runway(atcMediator);
+		atcMediator.registerFlight(sparrow101);
+		atcMediator.registerRunway(mainRunway);
+		sparrow101.getReady();
+		mainRunway.land();
+		sparrow101.land();
+	}
+}
